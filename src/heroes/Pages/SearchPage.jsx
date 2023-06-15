6cd8240a5b0997442160a5fadfd0afc9f2842888
Christian Martinez <dantedeat@hotmail.com>
2023-06-14 22:46:07 -0600

    chan

--- src/heroes/Pages/SearchPage.jsx
+++ src/heroes/Pages/SearchPage.jsx
@@ -1,8 +1,52 @@
+import { HeroCard } from "../components/HeroCard"
 
 export const SearchPage = () => {
   return (
     <>
-    <h1>SearchPage</h1>
+    <h1>Search</h1>
+    <hr />
+
+<div className="row">
+          <div className="col-5">
+            <h4>Searching</h4>
+            <hr />
+
+            <form>
+              <input 
+                type="text"
+                placeholder="Search a Hero"
+                className="form-control"
+                name="searchText"
+                autoComplete="off"
+              />
+
+              <button className="btn btn-outline-primary mt-1">
+                Search
+              </button>
+
+
+            </form>
+
+          </div>
+
+          <div className="col-7">
+            <h4>Result</h4>
+            <hr />
+          
+
+          <div className="alert alert-primary">
+            Search Hero
+          </div>
+
+          <div className="alert alert-danger">
+            there are not <b>hero</b>
+          </div>
+
+          <HeroCard />
+
+
+        </div>
+    </div>
     </>
   )
 }
